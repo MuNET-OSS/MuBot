4867f296c2a4c1ae414fef4b0a3240be501b8581
Clansty <i@gao4.pw>
2025-11-25 13:39:36 +0800

    fix 图片定位

--- apps/web/src/components/Bg.tsx
+++ apps/web/src/components/Bg.tsx
@@ -3,7 +3,7 @@ import _ from "lodash";
 
 export default component$(()=>{
 	return <div class="absolute bottom-0 left-0 right-0 w-full h-max z--1 of-hidden">
-		<img src="https://web-assets.c5y.moe/B50/bg.png" class="h-max w-full" />
+		<img src="https://web-assets.c5y.moe/B50/bg.png" class="block h-max w-full" />
 		<img src="https://web-assets.c5y.moe/B50/fish.png" class="absolute bottom-0 left-0 right-0 w-full h-max" style={{
 			transform: `translateY(${_.random(-75, 50)}px) translateX(${_.random(-150, 50)}px) rotate(${_.random(-5, 5)}deg)`,
 		}} />
